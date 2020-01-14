e5c58907648e235e4974df68874518227695c84a
hidanscript <hidanshira2@gmail.com>
2020-01-13 22:51:01 -0300

    Se agrego contenido al boton Agregar

--- src/Screens/AddScreen.js
+++ src/Screens/AddScreen.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import './css/AddScreen.css';
+
 
 function AddScreen() {
     return(
-        <h2>Add Post</h2>
+        <div className="add-screen">
+            <h2>Add Post</h2>
+            <form className="add-post-form">
+                <input className="add-input" type="text" placeholder="Titulo del post" />
+                <textarea placeholder="Descripción del post" rows="28" />
+                <input className="btn-submit" type="submit" value="Confirmar"/>
+            </form>
+            
+        </div>
     );
 }
 
